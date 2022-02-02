b3d6e70b8215316735c75eb269184b94a3ddaf0a
Guilherme Otsu <guiotsu@gmail.com>
2022-02-02 18:54:40 +0000

    automock hehe

---
+++ 01-teste-unidade/2.2-features/Feature.Tests/AutoMock/CustomerServiceAutoMockTests.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using Feature.Customer;
+using Moq.AutoMock;
+using Tests.Fixtures;
+using Xunit;
+
+namespace Feature.Tests.AutoMock
+{
+		public class CustomerServiceAutoMockTests
+		{
+				private CustomerTestFixture _customerFixture;
+
+				public CustomerServiceAutoMockTests(
+								CustomerTestFixture  customerTestFixture)
+				{
+						_customerFixture = customerTestFixture;
+				}
+
+				[Fact]
+				public void ClienteService_Add_MustBeValid()
+				{
+						// Arrange
+						var clientes = _customerFixture.GenerateValidCustomer();
+						var mocker = new AutoMocker();
+						var clienteService = mocker.CreateInstance<CustomerService>();
+
+						// Act
+						clienteService.Add(clientes.First());
+
+						// Assert
+				}
+		}
+}
